6cfaddc6cb4e61b4405a8809927566f649c35cf6
Christian Albert <calbert1209@gmail.com>
2026-01-14 22:37:54 +0900

    simplify config

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,8 @@ import preact from "@preact/preset-vite";
 export default defineConfig(({ command }) => {
   const baseConfig = {
     plugins: [preact()],
-    build: {
-      outDir: "dist",
-      sourcemap: true,
-    },
     test: {
       globals: true,
-      environment: "jsdom",
     },
   };
 
