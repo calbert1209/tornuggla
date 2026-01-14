8469653bea1e352f6df5b761bd7878a4283a0bb1
Christian Albert <calbert1209@gmail.com>
2026-01-14 22:26:09 +0900

    configure base for production only

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
 
-export default defineConfig({
-  plugins: [preact()],
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  },
-  base: '/tornuggla/',
-  server: {
-    open: true
+export default defineConfig(({ command }) => {
+  const baseConfig = {
+    plugins: [preact()],
+    build: {
+      outDir: "dist",
+      sourcemap: true,
+    },
+    test: {
+      globals: true,
+      environment: "jsdom",
+    },
+  };
+
+  if (command === "serve") {
+    return baseConfig;
   }
-})
+
+  return {
+    ...baseConfig,
+    base: "/tornuggla/",
+  };
+});
